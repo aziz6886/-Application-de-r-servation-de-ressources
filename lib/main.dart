3467552b72a1ea5c334de4bf4d791b80cb4ba030
Aziz <aziztouati0806@gmail.com>
2025-11-25 22:19:38 +0100

    4th comm

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Resource Booking',
+      title: 'Reservation',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
